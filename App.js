3c9c3ac7c7c7ee7c0cf889865441ef156da9126f
crystalize <70881846+crystalize-dev@users.noreply.github.com>
2022-09-25 16:58:09 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./app.css"
+
+function App() {
+    return (
+        <div className="wrapper">
+            <div className="road">
+                <div className="stick" data-color={"red"}/>
+            </div>
+        </div>
+    );
+}
+
+export default App;
